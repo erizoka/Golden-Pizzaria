b62445087cc766c41a68d4911dba058950e99f1d
Erica Esteves <ericaesteves95@gmail.com>
2024-02-10 10:07:20 -0300

    backend done, starting frontend

---
+++ backend/src/controllers/user/CreateUserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response, response } from "express";
+import { CreateUserService } from '../../services/user/CreateUserService'
+
+class CreateUserController {
+    async handle(req: Request, res: Response){
+        const { name, email, password } = req.body;
+
+        const createUser = new CreateUserService();
+        
+        const user = await createUser.execute({
+            name,
+            email,
+            password
+        });
+
+        return res.json(user)
+    }
+}
+
+export { CreateUserController }
